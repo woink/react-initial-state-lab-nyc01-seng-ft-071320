1214c779007af16c7acced1c033d0a65a5c217e5
woink <wardprice@gmail.com>
2020-09-14 14:45:53 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React from 'react'
+
+
+class Bomb extends React.Component {
+
+  constructor(props) {
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  
+  render() {
+    const alert = this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`
+    
+    return (
+      <div>
+        {alert}
+      </div>
+    )
+  }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,24 @@
 // your ImageSlider code here!
+import React from 'react'
+
+class ImageSlider extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+
+    const alert = `I am on slide ${this.state.currentSlideIndex}`
+    return (
+      <div>
+        {alert}
+      </div>
+      )
+  }
+}
+
+export default ImageSlider
